fe3f9bbbe4a3e9e94726b29f276d9b2a8ecc6b4f
Youssef Alnemr <yousef.nemr15@gmail.com>
2023-12-16 22:55:39 +0200

    [Rust] Implement generics solutions

--- generics/generics2.rs
+++ generics/generics2.rs
@@ -8,13 +8,13 @@
 
 // I AM NOT DONE
 
-struct Wrapper {
-    value: u32,
+struct Wrapper<T> {
+    value: T
 }
 
-impl Wrapper {
-    pub fn new(value: u32) -> Self {
-        Wrapper { value }
+impl<T> Wrapper<T> {
+    pub fn new(value: T) -> Self {
+        Wrapper::<T> { value }
     }
 }
 
@@ -24,11 +24,11 @@ mod tests {
 
     #[test]
     fn store_u32_in_wrapper() {
-        assert_eq!(Wrapper::new(42).value, 42);
+        assert_eq!(Wrapper::<i32>::new(42).value, 42);
     }
 
     #[test]
     fn store_str_in_wrapper() {
-        assert_eq!(Wrapper::new("Foo").value, "Foo");
+        assert_eq!(Wrapper::<&str>::new("Foo").value, "Foo");
     }
 }
