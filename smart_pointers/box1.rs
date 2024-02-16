2b293397aada7f79cdc81c4f3bace82a16c665c3
Youssef Alnemr <20484975+Nnemr@users.noreply.github.com>
2024-02-16 18:39:07 +0200

    Implement Solutions for Smart pointers (#9)
    * [smart pointers] Solve arc1.rs
    
    * [smart pointers] Solve box1
    
    * [smart pointers] Solve cow1
    
    * [smart pointers] Solve rc1
    
    * [smart pointers] Cleanup TODOs and I AM NOT DONE

--- smart_pointers/box1.rs
+++ smart_pointers/box1.rs
@@ -18,12 +18,10 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
-    Nil,
+    Cons(i32, Box::<List>),
+    Nil
 }
 
 fn main() {
@@ -35,11 +33,11 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    return List::Nil;
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    return List::Cons(4, Box::new(List::Nil));
 }
 
 #[cfg(test)]
